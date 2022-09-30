1ccc1d7f6db8642b35dfe6020f9eb352eef232a2
Gunjan Yadav <gunjan4699@gmail.com>
2022-09-30 10:52:07 +0530

    Day1_prob1

---
+++ Day1/runningSum.java
@@ -0,0 +1,24 @@
+package Day1;
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int[] output = new int[nums.length];
+        for(int i=0; i<nums.length; i++){
+            if(i==0)
+                output[i] = nums[i];
+            else
+                output[i] = output[i-1]+nums[i];
+        }
+        return output;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {1,2,3,4};
+        Solution solution = new Solution();
+        int[] output = solution.runningSum(nums);
+        for (int i : output) {
+            System.out.print(i+" ");
+        }
+
+    }
+}
+
