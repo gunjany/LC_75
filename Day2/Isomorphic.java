3002c7a60d736b96799a0e1b2193438b465d902f
Gunjan Yadav <gunjan4699@gmail.com>
2022-10-03 10:19:47 +0530

    day6_updates

---
+++ Day2/Isomorphic.java
@@ -0,0 +1,43 @@
+package Day2;
+import java.util.HashMap;
+
+public class Isomorphic {
+    static class Solution {
+        public boolean isIsomorphic(String s, String t) {
+            HashMap<Character, Character> mapping = new HashMap<Character, Character>();
+            HashMap<Character, Boolean> mappedBefore = new HashMap<Character, Boolean>();
+            Boolean equivalent = true;
+
+            for (int i = 0; i < s.length(); i++) {
+                if (mapping.containsKey(s.charAt(i))) {
+                    char mapped = mapping.get(s.charAt(i));
+
+                    if (mapped != t.charAt(i)) {
+                        equivalent = false;
+                        break;
+                    }
+                }
+
+                else {
+                    if (mappedBefore.containsKey(t.charAt(i))) {
+                        equivalent = false;
+                        break;
+                    } else {
+                        mappedBefore.put(t.charAt(i), true);
+                        mapping.put(s.charAt(i), t.charAt(i));
+                    }
+
+                }
+            }
+            return equivalent;
+
+        }
+    }
+        public static void main(String[] args) {
+            Solution solution = new Solution();
+            String s = "badc"; // p = t; a = i; p = t; e = l; r = e || b=b; a=a; d=b; c=a
+            String t = "baba";
+            System.out.println(solution.isIsomorphic(t, s));
+        }
+    
+}
---
+++ Day6/PreOrderTraversal.java
@@ -0,0 +1,48 @@
+package Day6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PreOrderTraversal {
+
+    static class Node {
+        public int val;
+        public List<Node> children = new ArrayList<>();
+    
+        public Node() {}
+    
+        public Node(int _val) {
+            val = _val;
+        }
+    
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    };
+
+    
+    static class Solution {
+        List<Integer> ans = new ArrayList<>();
+        public List<Integer> preorder(Node root) {
+            if(root == null) return ans;
+            ans.add(root.val);
+            for(Node child: root.children)
+                preorder(child);
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        Node root = new Node(1);
+        (root.children).add(new Node(3));
+        root.children.add(new Node(2));
+        root.children.add(new Node(4));
+        root.children.get(0).children.add(new Node(5));
+        root.children.get(0).children.add(new Node(6));
+        System.out.println(solution.preorder(root));
+
+    }
+    
+}
