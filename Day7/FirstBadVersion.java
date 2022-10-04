5923a799e47698bfcdb2c680ae12ef83e31ae942
Gunjan Yadav <gunjan4699@gmail.com>
2022-10-04 11:42:15 +0530

    Day7_prob2_application of BS

---
+++ Day7/FirstBadVersion.java
@@ -0,0 +1,36 @@
+package Day7;
+
+public class FirstBadVersion {
+
+    /* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+      public static class Solution extends VersionControl {
+        public int firstBadVersion(int n) {
+            int start = 1;
+            int end = n;
+            int ans = -1;
+            
+            while(start<=end){
+                int mid = start+(end-start)/2;
+                
+                if(isBadVersion(mid)){
+                    ans = mid;
+                    end = mid-1;
+                }else{
+                    start = mid+1;
+                }
+            }
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.firstBadVersion(5));
+    }
+    
+}
---
+++ Day7/VersionControl.java
@@ -0,0 +1,12 @@
+package Day7;
+
+public class VersionControl {
+
+    Boolean[] version = {false, false, false, true, true};
+
+    public Boolean isBadVersion(int n){
+        return version[n];
+
+    }
+
+}
